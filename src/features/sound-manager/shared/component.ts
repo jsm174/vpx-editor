8578d7a20cb489edf835cd85acd2cbe04923fb1f
Jason Millard <jsm174@gmail.com>
2026-06-10 20:09:51 -0400

    deps: bump vpin to 0.26.7

--- src/features/sound-manager/shared/component.ts
+++ src/features/sound-manager/shared/component.ts
@@ -464,7 +464,6 @@ export function initSoundManagerComponent(
         sounds.push({
           name,
           path: file.name,
-          internal_name: '',
           fade: 0,
           volume: 0,
           balance: 0,
@@ -518,7 +517,6 @@ export function initSoundManagerComponent(
       sounds.push({
         name: result.name,
         path: result.originalPath || '',
-        internal_name: '',
         fade: 0,
         volume: 0,
         balance: 0,
